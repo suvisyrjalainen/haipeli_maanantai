bc273fdaef8eee64b8b13b72fb7166d2a4509e0e
Suvi Syrjalainen <suvi.syrjalainen@gmail.com>
2024-02-26 16:04:39 +0200

    Pelaajan liikkuminen

--- Haipeli/Assets/Script/Player/Player.cs
+++ Haipeli/Assets/Script/Player/Player.cs
@@ -1,11 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+    // Pelaajan liikkumisnopeus ns liikkumisen kerroin
     public float moveSpeed = 5f;
+
     public Rigidbody2D body;
+
+    // Tämä kuuntelee kaikki pelaajan wasd ja nuolinäppäimien painallukset
+    private Master controls;
+    // Tähän tallennetaan nappien painallukset x ja y akselilla
+
+    private Vector2 moveInput;
+    
+    // Tämä suoritetaan ennen pelin käynnistystä
+    private void Awake()
+    {
+        controls = new Master();
+    }
     
     // Start is called before the first frame update
     void Start()
@@ -18,4 +33,19 @@ public class Player : MonoBehaviour
     {
         
     }
+
+    private void OnEnable(){
+        controls.Enable();
+    }
+
+    private void FixedUpdate(){
+        Move(); 
+    }
+
+    private void Move(){
+        moveInput = controls.Player.Move.ReadValue<Vector2>();
+        Vector2 movement = new Vector2(moveInput.x, moveInput.y) * moveSpeed * Time.fixedDeltaTime;
+        body.MovePosition(body.position + movement);
+    }
+
 }
