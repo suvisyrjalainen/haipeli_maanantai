61541bdcaf0e17d165fc25e24165df67f4429991
Suvi Syrjalainen <suvi.syrjalainen@gmail.com>
2024-04-15 17:01:56 +0300

    game managerin alku 2

--- Haipeli/Assets/Script/Managers/GameManager.cs
+++ Haipeli/Assets/Script/Managers/GameManager.cs
@@ -6,7 +6,29 @@ public class GameManager : MonoBehaviour
 {
 
     public static GameManager instance;
+
+    public gameStates currentGameState;
     // Start is called before the first frame update
+    
+
+    void Awake()
+    {
+        //Sinletonin idea on että tätä komponenttia on vain yksi per peli
+        //Tällä varmistetaan, että pelissä on vain yksi GameManager
+        if (instance == null)
+        {
+            instance = this;
+            //Tämä säilyy kun vaihdetaan sceneä
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    
+    
+    
     void Start()
     {
         
